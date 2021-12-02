a4571769c5a8aee23e6a990a65d549077f3b95af
Jaewon Kim <89503906+wodnjse@users.noreply.github.com>
2021-12-02 10:14:24 +0900

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(){
+	int arr[] = {5,3,1,6,8,7,2,4};
+	int i, j;
+	
+	for(i = 0; i <8; i++){
+		for(j = 0; j < 8-i-1; j++){
+			if(arr[j] > arr[j+1]){
+				int temp = arr[j];
+				arr[j] =  arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	for(i=0;i<8;i++){
+		printf("%d\t",arr[i]);
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*	for(i=0;i < 8;i++){
+		for(j = 0;j < 8-1-i;j++){
+			if(arr[j] > arr[j+1]){
+				int tmp = arr[j];  // tmp = arr[j], arr[j] = arr[j], arr[j+1] = arr[j+1]
+				arr[j] = arr[j+1]; // tmp = arr[j], arr[j] = arr[j+1], arr[j+1] = arr[j+1]
+				arr[j+1] = tmp;    // arr[j] = arr[j+1], arr[j+1] = tmp
+			}	
+		}
+	}
+	
+	for(int i = 0; i < 8 ; i++){
+		printf("%d\t",arr[i]);
+	}
+	
+}*/
+
