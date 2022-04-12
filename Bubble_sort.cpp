21d721f34aaa0f92ebdcb4c1d8a82876a4d2b770
Jaewon Kim <89503906+wodnjse@users.noreply.github.com>
2022-04-13 00:21:12 +0900

    Update Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -17,20 +17,3 @@ int main(){
 		printf("%d\t",arr[i]);
 	}
 }
-
-/*	for(i=0;i < 8;i++){
-		for(j = 0;j < 8-1-i;j++){
-			if(arr[j] > arr[j+1]){
-				int tmp = arr[j];  // tmp = arr[j], arr[j] = arr[j], arr[j+1] = arr[j+1]
-				arr[j] = arr[j+1]; // tmp = arr[j], arr[j] = arr[j+1], arr[j+1] = arr[j+1]
-				arr[j+1] = tmp;    // arr[j] = arr[j+1], arr[j+1] = tmp
-			}	
-		}
-	}
-	
-	for(int i = 0; i < 8 ; i++){
-		printf("%d\t",arr[i]);
-	}
-	
-}*/
-
